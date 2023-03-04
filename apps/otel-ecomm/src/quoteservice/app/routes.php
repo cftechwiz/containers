57ff82e091f838f4515ef791cd27c86f05534c15
CTech <colinfallwell@gmail.com>
2023-03-04 15:40:43 +0100

    Add OpenTelemetry

---
+++ apps/otel-ecomm/src/quoteservice/app/routes.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+use OpenTelemetry\API\Common\Instrumentation\Globals;
+use OpenTelemetry\API\Trace\Span;
+use OpenTelemetry\API\Trace\SpanKind;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+
+function calculateQuote($jsonObject): float
+{
+    $quote = 0.0;
+    $childSpan = Globals::tracerProvider()->getTracer('manual-instrumentation')
+        ->spanBuilder('calculate-quote')
+        ->setSpanKind(SpanKind::KIND_INTERNAL)
+        ->startSpan();
+    $childSpan->addEvent('Calculating quote');
+
+    try {
+        if (!array_key_exists('numberOfItems', $jsonObject)) {
+            throw new \InvalidArgumentException('numberOfItems not provided');
+        }
+        $numberOfItems = intval($jsonObject['numberOfItems']);
+        $quote = round(8.90 * $numberOfItems, 2);
+
+        $childSpan->setAttribute('app.quote.items.count', $numberOfItems);
+        $childSpan->setAttribute('app.quote.cost.total', $quote);
+
+        $childSpan->addEvent('Quote calculated, returning its value');
+    } catch (\Exception $exception) {
+        $childSpan->recordException($exception);
+    } finally {
+        $childSpan->end();
+        return $quote;
+    }
+}
+
+return function (App $app) {
+    $app->post('/getquote', function (Request $request, Response $response) {
+        $span = Span::getCurrent();
+        $span->addEvent('Received get quote request, processing it');
+
+        $body = $request->getBody()->getContents();
+        $jsonObject = json_decode($body, true);
+
+        $data = calculateQuote($jsonObject);
+
+        $payload = json_encode($data);
+        $response->getBody()->write($payload);
+
+        $span->addEvent('Quote processed, response sent back', [
+            'app.quote.cost.total' => $data
+        ]);
+
+        return $response
+            ->withHeader('Content-Type', 'application/json');
+    });
+};
